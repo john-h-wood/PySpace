eabe83c4c5e5f12a6a4aeed5cbb3a2169face5f4
John Wood <113411200+john-h-wood@users.noreply.github.com>
2024-01-23 00:55:06 -0500

    Investigation into values like 987654321/123456789 in base 10, but in other bases

---
+++ base_convert/base_convert.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as sps
+
+START_BASE = 2
+END_BASE = 125
+
+# ============ CALCULATION ============
+
+def digits_to_decimal(base, digits):
+    base_list = np.ones(len(digits)) * base
+    base_powers = np.power(base_list, np.arange(len(digits) - 1, -1, -1))
+    return np.dot(digits, base_powers)
+
+
+def quotient(base):
+    den = digits_to_decimal(base, np.arange(1, base))
+    num = digits_to_decimal(base, np.arange(base - 1, 0, -1))
+    return num / den
+
+
+# ============ PLOTTING ============
+x = list(range(START_BASE, END_BASE + 1))
+y = [quotient(i) for i in x]
+
+plt.figure(figsize=(6.4, 4.8))
+plt.plot(x, y)
+plt.xlabel('Base')
+plt.ylabel('Quotient')
+plt.savefig('plot.png')
+
+# ============ LINEAR REGRESSION ===========
+regression = sps.linregress(x, y)
+print(regression)
+print(f'r-squared: {regression[2] ** 2}')
